b274776c7a48ea4b36f320dca0f27bb4fe4ed04e
jayuchawla <jayprakashchawla78@gmail.com>
2020-05-10 21:21:53 +0530

    Updates

---
+++ precache-manifest.cdec876796870d19fb4023152664cc61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "305698dd18063ebee809d69f289ce88d",
+    "url": "/jayuchawla/to-do/index.html"
+  },
+  {
+    "revision": "8dfe83cedfd79d93e216",
+    "url": "/jayuchawla/to-do/static/css/main.768612c9.chunk.css"
+  },
+  {
+    "revision": "472c1de61e1acfb205a8",
+    "url": "/jayuchawla/to-do/static/js/2.a1cf833a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jayuchawla/to-do/static/js/2.a1cf833a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dfe83cedfd79d93e216",
+    "url": "/jayuchawla/to-do/static/js/main.d5415fa3.chunk.js"
+  },
+  {
+    "revision": "b5950d9d838c33bad9e9",
+    "url": "/jayuchawla/to-do/static/js/runtime-main.9e5fd281.js"
+  }
+]);
